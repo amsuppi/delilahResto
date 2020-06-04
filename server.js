c6fd3042f3838f244f459ce10d7a65ccc66ee59e
Maribel <maribelsuppi97@gmail.com>
2020-06-04 19:16:49 -0300

    Primeras consultas

--- server.js
+++ server.js
@@ -1,10 +1,57 @@
-const $sequelize = require('sequelize');
-const sequelize = new $sequelize('mysql://mibd:root@localhost/data_base');
-
 const express = require('express');
-const server = express();
+const bodyParser = require('body-parser');
+const app = express();
 
-server.listen(3000, ()=>{
-    console.log ("Servidor Inicializado..");
+const $sequelize = require('sequelize');
+const sequelize = new $sequelize('mysql://mari:suppi@localhost:8889/data_base');
+
+const resultado = sequelize.query(
+    `SELECT * FROM productos`,
+    { raw: true },
+);
+
+//----------------------------------------------------------------------
+
+app.use(bodyParser.json());
+
+app.get("/productos", (req, res, next)=>{
+
+    res.json(resultado);
+    next();
+});
+
+app.put('/productos/:id', (req, res)=>{
+    const title = req.body.title;
+    const price = req.body.price;
+    const id = req.body.id;
+
+        sequelize.query(
+            `UPDATE productos SET title = ?, price = ? WHERE id = ?`,
+            {replacements:[title, price, id]})
+        
+        .then( res.json("actualizado"))
+    
+});
+
+app.post('/productos', (req, res) =>{
+
+    const title = req.body.title;
+    const price = req.body.price;
+
+    sequelize.query(`
+        INSERT INTO productos (title, price) VALUES ( ?, ?)`,
+        {replacements: [title, price]
+
+    }).then(res.json("Agregado"))
+
+});
+
+
+
+app.listen(3000,()=>{
+    console.log('servidor inicalizado');
+   
 })
+
+
 
